212c13b67dd2e3fd504522346659de9bb6a41841
MeowQAQ <1259053332@qq.com>
2019-10-03 22:14:44 +0800

    update file

--- Lintcode/Lintcode.c
+++ Lintcode/Lintcode.c
@@ -41,11 +41,56 @@ public:
 class Question_3
 {
 	//计算数字 k 在 0 到 n 中的出现的次数，k 可能是 0~9 的一个值。
+	public:
 	    int digitCounts(int k, int n) 
 	    {
-        	
+	    	int a = 0;
+	   		
+        	for (int m = 0; m <=n; ++m)
+        	{
+        		int x = m;
+	    		if(x == k)
+	    		{	
+	    			a++;
+	    		}
+	    		else if(x>9)
+		    	{
+			    	int t = 1;
+		   			int i = 0;
+		   			
+		   			//判断位数
+		   			while(x/t!=0)
+		   			{	
+		   				i++;
+		   				t =t*10;
+		   			}
+
+		   			// for (int j = 1; j < i; ++j)
+		   			// {
+		   			// 	r = r*10;
+		   			// }
+		   			
+		   			for (int j = 1; j < i; ++j)
+		   			{
+		   				int r = 10;
+		   				int z = x%r;
+		   				x = (x-z)/r;
+		   				if(x == k)
+		   					a++;
+		   				if(z == k)
+		   					a++;
+		   				
+
+		   			}
+
+		    	}
+        	}
+
+        	return a;
    		}
 };
+
+
 int main()
 {
 	Question_1 Q1;
@@ -56,5 +101,8 @@ int main()
 	cout << "第2题：输入105,返回其阶乘后末尾0的个数：\n";
 	cout << Q2.trailingZeros(105) << "\n";
 
+	Question_3 Q3;
+	cout << "第3题：计算数字 k 在 0 到 n 中的出现的次数，k 可能是 0~9 的一个值。：\n";
+	cout << Q3.digitCounts(2,302) << "\n";
 	return 0;
 }
